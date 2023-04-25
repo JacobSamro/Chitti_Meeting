a93ad114d3b446b613f737f0333a8b995aa1c7b8
hiphopsakthi <69505534+hiphopsakthi@users.noreply.github.com>
2023-04-25 10:59:18 +0530

    feat(constant): add api constants

---
+++ lib/common/constants/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static const String baseUrl = "https://live-automation.cloud.chitti.xyz";
+  static const String addParticipantUrl = "$baseUrl/api/workshops/participants";
+}
