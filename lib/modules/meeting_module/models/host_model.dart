5899fea586605f14430da72a8d153c76a59a7a79
hiphopsakthi <69505534+hiphopsakthi@users.noreply.github.com>
2023-06-07 17:53:35 +0530

    fix(host video): fix the video player

---
+++ lib/modules/meeting_module/models/host_model.dart
@@ -0,0 +1,6 @@
+class HostModel {
+  final String name;
+  final String src;
+
+  HostModel({required this.name,required this.src});
+}
--- lib/modules/view_module/widgets/custom_video_player.dart
+++ lib/modules/view_module/widgets/custom_video_player.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
-  const CustomVideoPlayer({super.key, required this.src});
+  const CustomVideoPlayer({super.key, required this.src, this.hieight = 200});
   final String src;
-
+  final double hieight;
   @override
   State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
 }
@@ -21,12 +21,13 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   initVideo() async {
     if (!locator.isRegistered<VideoPlayerController>()) {
       locator.registerLazySingleton<VideoPlayerController>(
-          () => VideoPlayerController.network(widget.src));
+        () => VideoPlayerController.network(widget.src),
+      );
       controller = locator<VideoPlayerController>();
       await controller.initialize();
       controller.play();
+      controller.setLooping(true);
       setState(() {});
-
       return;
     }
     controller = locator<VideoPlayerController>();
@@ -39,12 +40,15 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: double.infinity,
-      child: controller.value.isInitialized
-          ? VideoPlayer(controller)
-          : const Center(child: CircularProgressIndicator()),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: SizedBox(
+        height: widget.hieight,
+        width: double.infinity,
+        child: controller.value.isInitialized
+            ? VideoPlayer(controller)
+            : const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
