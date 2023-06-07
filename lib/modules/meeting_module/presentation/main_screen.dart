db72705aa2a4ad02942f056d5029c0f2cf2e8506
hiphopsakthi <69505534+hiphopsakthi@users.noreply.github.com>
2023-06-07 17:54:00 +0530

    refactor(code)

--- lib/modules/meeting_module/presentation/main_screen.dart
+++ lib/modules/meeting_module/presentation/main_screen.dart
@@ -1,16 +1,18 @@
 import 'package:chitti_meeting/common/widgets/custom_bottom_navigation.dart';
 import 'package:chitti_meeting/modules/chat_module/presentation/chat_screen.dart';
 import 'package:chitti_meeting/modules/meeting_module/presentation/participants_screen.dart';
-import 'package:chitti_meeting/modules/view_module/presentation/gallery_view.dart';
-import 'package:chitti_meeting/modules/view_module/presentation/speaker_view.dart';
+import 'package:chitti_meeting/modules/meeting_module/states/meeting_states.dart';
+import 'package:chitti_meeting/modules/view_module/providers/view_provider.dart';
+import 'package:chitti_meeting/modules/view_module/widgets/custom_video_player.dart';
 import 'package:chitti_meeting/services/locator.dart';
-import 'package:dyte_core/dyte_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:livekit_client/livekit_client.dart';
 import 'package:video_player/video_player.dart';
 
-import '../../view_module/presentation/standard_view.dart';
+import '../../view_module/presentation/view_screen.dart';
+import '../providers/meeting_provider.dart';
 
 class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
@@ -20,178 +22,244 @@ class MainScreen extends ConsumerStatefulWidget {
 }
 
 class _MainScreenState extends ConsumerState<MainScreen> {
-  final DyteMobileClient client = locator<DyteMobileClient>();
-  Widget currentScreen = const StandardView();
-  bool isFullScreen = false;
+  final Room room = locator<Room>();
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(participantProvider.notifier).addLocalParticipantTrack();
+    });
+  }
+
   @override
   void dispose() {
     super.dispose();
-    locator<VideoPlayerController>().dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    // final textTheme = Theme.of(context).textTheme;
+    final ViewType viewType = ref.watch(viewProvider);
     return Scaffold(
-      bottomNavigationBar: CustomBottomNavigation(
-        items: [
-          CustomBottomNavigationItem(
-            label: isFullScreen ? "Exit" : "Full Screen",
-            iconPath: "assets/icons/full_screen.png",
-          ),
-          CustomBottomNavigationItem(
-            label: client.localUser.videoEnabled ? "Video On" : "Video Off",
-            iconPath: client.localUser.videoEnabled
-                ? "assets/icons/video.png"
-                : "assets/icons/video_off.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Mic Off",
-            iconPath: "assets/icons/mic_off.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Chat",
-            iconPath: "assets/icons/message.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Switch View",
-            iconPath: "assets/icons/view.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Settings",
-            iconPath: "assets/icons/settings.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Leave",
-            iconPath: "assets/icons/call_outline.png",
-          ),
-          const CustomBottomNavigationItem(
-            label: "Participants",
-            iconPath: "assets/icons/people.png",
-          ),
-        ],
-        onChanged: (value) async {
-          final DyteLocalUser localUser = client.localUser;
-          switch (value) {
-            case "Video On":
-              localUser.videoEnabled = false;
-              setState(() {});
-              await localUser.disableVideo();
-              // ref.read(cameraProvider.notifier).toggleVideo();
-              break;
-            case "Video Off":
-              setState(() {
-                localUser.videoEnabled = true;
-              });
-              await localUser.enableVideo();
-              // ref.read(cameraProvider.notifier).toggleVideo();
-              break;
-            case "Mic On":
-              localUser.audioEnabled
-                  ? localUser.disableAudio()
-                  : localUser.enableAudio();
-              break;
-            case "Switch View":
-              showModalBottomSheet(
-                  context: context,
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      side: BorderSide(
-                          width: 1, color: Colors.white.withOpacity(0.1))),
-                  builder: (context) {
-                    return Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                currentScreen = const StandardView();
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: Text(
-                              "Standard View",
-                              style: textTheme.labelSmall,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 30),
-                            child: GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  currentScreen = const GalleryView();
-                                });
-                                Navigator.pop(context);
-                              },
-                              child: Text(
-                                "Gallery View",
-                                style: textTheme.labelSmall,
-                              ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                currentScreen = const SpeakerView();
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: Text(
-                              "Speaker View",
-                              style: textTheme.labelSmall,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-              break;
-            case "Chat":
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ChatScreen()));
-              break;
-            case "Settings":
-              break;
-            case "Participants":
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ParticipantsScreen()));
-              break;
-            case "Full Screen":
-              SystemChrome.setPreferredOrientations([
-                // DeviceOrientation.portraitUp,
-                DeviceOrientation.landscapeLeft,
-              ]);
-              setState(() {
-                currentScreen = const SpeakerView();
-                isFullScreen = true;
-              });
-              break;
-            case "Exit":
-              SystemChrome.setPreferredOrientations([
-                // DeviceOrientation.portraitUp,
-                DeviceOrientation.portraitUp,
-              ]);
-              setState(() {
-                currentScreen = const StandardView();
-                isFullScreen = false;
-              });
-              break;
-            case "Leave":
-              client.leaveRoom();
+      // appBar: AppBar(
+      //   title: Padding(
+      //     padding: const EdgeInsets.all(16),
+      //     child: Row(
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       children: [
+      //         Text(
+      //           room.name.toString(),
+      //           style: textTheme.bodySmall,
+      //         ),
+      //         const SizedBox(
+      //           width: 6,
+      //         ),
+      //         Container(
+      //           padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 6),
+      //           decoration: BoxDecoration(
+      //             color: Colors.white.withOpacity(0.1),
+      //             borderRadius: BorderRadius.circular(50),
+      //           ),
+      //           child: Text(
+      //             'time',
+      //             style: textTheme.displaySmall?.copyWith(fontSize: 10),
+      //           ),
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: viewType != ViewType.fullScreen
+          ? const Column(
+              children: [Expanded(child: ViewScreen()), NavigationBar()],
+            )
+          : SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  const CustomVideoPlayer(
+                      hieight: double.infinity,
+                      src:
+                          'https://streameggs.net/0ae71bda-4d2f-4961-9ced-e6d21ede69e6/master.m3u8'),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: const NavigationBar()),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
 
-              break;
-            default:
-              break;
-          }
-        },
-      ),
-      body: currentScreen,
+class NavigationBar extends ConsumerWidget {
+  const NavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final Room room = locator<Room>();
+    final ViewType type = ref.watch(viewProvider);
+    return CustomBottomNavigation(
+      items: [
+        CustomBottomNavigationItem(
+          label: type == ViewType.fullScreen ? "Exit" : "Full Screen",
+          iconPath: "assets/icons/full_screen.png",
+        ),
+        CustomBottomNavigationItem(
+          label: room.localParticipant!.isCameraEnabled()
+              ? "Video On"
+              : "Video Off",
+          iconPath: room.localParticipant!.isCameraEnabled()
+              ? 'assets/icons/video.png'
+              : "assets/icons/video_off.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Mic Off",
+          iconPath: "assets/icons/mic_off.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Chat",
+          iconPath: "assets/icons/message.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Switch View",
+          iconPath: "assets/icons/view.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Settings",
+          iconPath: "assets/icons/settings.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Leave",
+          iconPath: "assets/icons/call_outline.png",
+        ),
+        const CustomBottomNavigationItem(
+          label: "Participants",
+          iconPath: "assets/icons/people.png",
+        ),
+      ],
+      onChanged: (value) async {
+        switch (value) {
+          case "Video On":
+            await room.localParticipant?.setCameraEnabled(false);
+            // localUser.videoEnabled = false;
+            // setState(() {});
+            // await localUser.disableVideo();
+            // ref.read(cameraProvider.notifier).toggleVideo();
+            break;
+          case "Video Off":
+            room.localParticipant?.setCameraEnabled(true);
+            // setState(() {
+            //   localUser.videoEnabled = true;
+            // });
+            // await localUser.enableVideo();
+            // ref.read(cameraProvider.notifier).toggleVideo();
+            break;
+          case "Mic Off":
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Mic was disabled by the host")));
+            break;
+          case "Switch View":
+            showModalBottomSheet(
+                context: context,
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    side: BorderSide(
+                        width: 1, color: Colors.white.withOpacity(0.1))),
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(viewProvider.notifier)
+                                .changeViewType(ViewType.standard);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Standard View",
+                            style: textTheme.labelSmall,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 30),
+                          child: GestureDetector(
+                            onTap: () {
+                              ref
+                                  .read(viewProvider.notifier)
+                                  .changeViewType(ViewType.gallery);
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              "Gallery View",
+                              style: textTheme.labelSmall,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(viewProvider.notifier)
+                                .changeViewType(ViewType.speaker);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Speaker View",
+                            style: textTheme.labelSmall,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+            break;
+          case "Chat":
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const ChatScreen()));
+            break;
+          case "Settings":
+            break;
+          case "Participants":
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ParticipantsScreen()));
+            break;
+          case "Full Screen":
+            SystemChrome.setPreferredOrientations([
+              // DeviceOrientation.portraitUp,
+              DeviceOrientation.landscapeLeft,
+            ]);
+            ref.read(viewProvider.notifier).changeViewType(ViewType.fullScreen);
+            break;
+          case "Exit":
+            SystemChrome.setPreferredOrientations([
+              // DeviceOrientation.portraitUp,
+              DeviceOrientation.portraitUp,
+            ]);
+            ref.read(viewProvider.notifier).changeViewType(ViewType.standard);
+
+            break;
+          case "Leave":
+            room.disconnect();
+            ref.invalidate(participantProvider);
+            locator<VideoPlayerController>().dispose();
+            locator.unregister<VideoPlayerController>();
+            break;
+          default:
+            break;
+        }
+      },
     );
   }
 }
--- lib/modules/meeting_module/presentation/participants_screen.dart
+++ lib/modules/meeting_module/presentation/participants_screen.dart
@@ -1,60 +1,51 @@
-import 'package:dyte_core/dyte_core.dart';
+import 'package:chitti_meeting/modules/meeting_module/providers/meeting_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../services/locator.dart';
-
-class ParticipantsScreen extends StatelessWidget {
+class ParticipantsScreen extends ConsumerWidget {
   const ParticipantsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final DyteMobileClient client = locator<DyteMobileClient>();
+  Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
-    // final length = client.activeStream.length;
+    final participants = ref.watch(participantProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Participants'),
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.black,
-        actions: [TextButton(onPressed: () {}, child: const Text('X'))],
-      ),
-      body: StreamBuilder(
-          stream: client.activeStream,
-          builder: (context, snap) {
-            if (snap.hasData) {
-              final participants = snap.data;
-              return ListView.builder(
-                  itemCount: participants?.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(0.05),
-                                borderRadius: BorderRadius.circular(50)),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            participants![index].name,
-                            style: textTheme.labelSmall?.copyWith(
-                                color: Colors.white.withOpacity(0.75)),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
-    );
+        appBar: AppBar(
+          title: const Text('Participants'),
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.black,
+          actions: [TextButton(onPressed: () {}, child: const Text('X'))],
+        ),
+        body: ListView.builder(
+            itemCount: participants.length,
+            itemBuilder: (context, index) {
+              // final participant = participants[index] as Participant;
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.05),
+                          borderRadius: BorderRadius.circular(50)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      participants[index].name == '' ||
+                              participants[index].name == null
+                          ? participants[index].identity
+                          : participants[index].name,
+                      style: textTheme.labelSmall
+                          ?.copyWith(color: Colors.white.withOpacity(0.75)),
+                    ),
+                  ],
+                ),
+              );
+            }));
   }
 }
