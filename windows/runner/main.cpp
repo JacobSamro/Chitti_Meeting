6923bea396baaad352ea941e2ae854a0102a055a
SakthiShivan <69505534+hiphopsakthi@users.noreply.github.com>
2023-08-22 11:53:35 +0530

    fix: Package Name changed

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,11 +1,10 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
-
 #include "flutter_window.h"
 #include "utils.h"
-
 #include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+
 auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP);
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
@@ -30,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"chitti_meeting", origin, size)) {
+  if (!window.Create(L"Chitti Meet", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
