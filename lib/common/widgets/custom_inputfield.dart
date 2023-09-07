c096a550cc945383876547681a95792271a53d63
SakthiShivan <69505534+hiphopsakthi@users.noreply.github.com>
2023-09-07 18:15:55 +0530

    fix: add show/hide pasword option

--- lib/common/widgets/custom_inputfield.dart
+++ lib/common/widgets/custom_inputfield.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../services/responsive.dart';
 
-class CustomInputField extends StatelessWidget {
+class CustomInputField extends StatefulWidget {
   const CustomInputField({
     super.key,
     required this.controller,
@@ -14,6 +14,18 @@ class CustomInputField extends StatelessWidget {
   final bool obscureText;
 
   @override
+  State<CustomInputField> createState() => _CustomInputFieldState();
+}
+
+class _CustomInputFieldState extends State<CustomInputField> {
+  bool obscureText = false;
+  @override
+  void initState() {
+    super.initState();
+    obscureText = widget.obscureText;
+  }
+
+  @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final responsiveDevice = Responsive().getDeviceType(context);
@@ -21,39 +33,58 @@ class CustomInputField extends StatelessWidget {
       width: responsiveDevice != ResponsiveDevice.mobile ? 480 : 300,
       height: responsiveDevice != ResponsiveDevice.mobile ? 48 : 52,
       child: TextField(
-          controller: controller,
-          obscureText: obscureText,
-          textAlign: TextAlign.center,
-          style: textTheme.labelSmall
-              ?.copyWith(color: Colors.white, fontWeight: FontWeight.normal),
-          decoration: InputDecoration(
-            hintText: label,
-            hintStyle: textTheme.labelSmall
-                ?.copyWith(color: Colors.white.withOpacity(0.5)),
-            filled: true,
-            fillColor: Colors.white.withOpacity(0.1),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14),
-              borderSide: BorderSide(
-                color: Colors.white.withOpacity(0.2),
-                width: 0.5,
-              ),
+        controller: widget.controller,
+        obscureText: obscureText,
+        textAlign: TextAlign.center,
+        style: textTheme.labelSmall
+            ?.copyWith(color: Colors.white, fontWeight: FontWeight.normal),
+        decoration: InputDecoration(
+          hintText: widget.label,
+          suffixIcon: widget.obscureText
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          obscureText = !obscureText;
+                        });
+                      },
+                      child: Image.asset(
+                        obscureText
+                            ? 'icons/hide_password.png'
+                            : 'icons/show_password.png',
+                        width: 24,
+                        height: 24,
+                      )),
+                )
+              : null,
+          hintStyle: textTheme.labelSmall
+              ?.copyWith(color: Colors.white.withOpacity(0.5)),
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.1),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(0.2),
+              width: 0.5,
             ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14),
-              borderSide: BorderSide(
-                color: Colors.white.withOpacity(0.2),
-                width: 0.5,
-              ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(0.2),
+              width: 0.5,
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14),
-              borderSide: const BorderSide(
-                color: Colors.white,
-                width: 0.5,
-              ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: const BorderSide(
+              color: Colors.white,
+              width: 0.5,
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
